143b71f6e1bb4669d28a92300e2026faf058005c
Yutaka HARA <yutaka.hara+github@gmail.com>
2016-08-13 22:23:44 +0900

    add test

--- spec/type_check/invalid_ctor_call_spec.rb
+++ spec/type_check/invalid_ctor_call_spec.rb
@@ -15,5 +15,18 @@ describe FjR::TypeChecker do
         EOD
       }.to raise_error(TC::ArityError)
     end
+
+    it "argument type mismatch" do
+      expect {
+        check <<-EOD
+          class A extends Object { A(){ super(); } }
+          class B extends Object {
+            A my_a;
+            B(A a){ super(); this.my_a = a; }
+          }
+          new Object(new Object())
+        EOD
+      }.to raise_error(TC::ArityError)
+    end
   end
 end
