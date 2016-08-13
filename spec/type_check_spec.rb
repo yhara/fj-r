15a5e0c19a43191fc3c803cccdcedbcd64eaf200
Yutaka HARA <yutaka.hara+github@gmail.com>
2016-08-14 00:37:20 +0900

    spec: merged files

--- spec/type_check_spec.rb
+++ spec/type_check_spec.rb
@@ -14,4 +14,27 @@ describe FjR::TypeChecker do
       }.to raise_error(FjR::Program::ArityError)
     end
   end
+
+  context "invalid ctor call" do
+    it "too many arguments" do
+      expect {
+        TC.check <<-EOD
+          new Object(new Object())
+        EOD
+      }.to raise_error(TC::ArityError)
+    end
+
+    it "argument type mismatch" do
+      expect {
+        TC.check <<-EOD
+          class A extends Object { A(){ super(); } }
+          class B extends Object {
+            A my_a;
+            B(A a){ super(); this.my_a = a; }
+          }
+          new Object(new Object())
+        EOD
+      }.to raise_error(TC::ArityError)
+    end
+  end
 end
